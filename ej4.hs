54c58031b95dc40ac63e314d78036144aeef3184
Gaston Hamu <gastonhamu@gmail.com>
2023-04-23 20:01:30 -0300

    termine el 4

--- ej4.hs
+++ ej4.hs
@@ -7,7 +7,10 @@ main = do {
 
 sumaPrimerosNImpares :: Integer -> Integer
 -- Completar la definición de la función
-
+sumaPrimerosNImpares n = sumaPrimerosNImparesAux n 1
 -- Pueden agregan las funciones que consideren necesarias
 
-
+sumaPrimerosNImparesAux:: Integer -> Integer -> Integer
+sumaPrimerosNImparesAux n i| i > 2*n - 1 = 0
+                           | mod i 2 == 1 = (2*i + 2) + sumaPrimerosNImparesAux n (i+1)
+                           | otherwise= 0
